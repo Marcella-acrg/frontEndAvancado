49ad95d57ec018225e34d429d559f046cae791ed
Marcella <marcellaacoelho@gmail.com>
2025-05-02 20:55:18 -0300

    Desafios aula 04

---
+++ aula04/src/components/Exercicio1.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Exercicio1 = () => {
+  return (
+    <div>
+        <p>Isso é um texto!</p>
+        <img src="tigre.jpg"/>
+    </div>
+  )
+}
+
+export default Exercicio1
